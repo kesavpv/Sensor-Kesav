21e6ab21e040bb376f2f62a50cf98a087c796dc3
kesav kadalazhi <kesavpv>
2015-11-09 15:54:21 -0700

    week 6 gyro

--- Sensor-Kesav/src/org/usfirst/frc/team1165/robot/Robot.java
+++ Sensor-Kesav/src/org/usfirst/frc/team1165/robot/Robot.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team1165.robot;
 
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
@@ -25,110 +26,110 @@ import com.ni.vision.NIVision.ShapeMode;
  */
 public class Robot extends IterativeRobot
 {
-    
-    public static final Accelerometer accelerometer = new Accelerometer(); 
-    public static final AbsoluteEncoder absoluteEncoder = new AbsoluteEncoder();
+
+	public static final Accelerometer accelerometer = new Accelerometer(); 
+	public static final AbsoluteEncoder absoluteEncoder = new AbsoluteEncoder();
 	public static final VexRangeFinderK vexRangeFinderK = new VexRangeFinderK();
-    public static OI oi;
-	
+	public static OI oi;
+
 	int session;
-    Image frame;
+	Image frame;
 
 	Command autonomousCommand;
 
-    /**
-     * This function is run when the robot is first started up and should be
-     * used for any initialization code.
-     */
-    public void robotInit()
-    {
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+	public void robotInit()
+	{
 		oi = new OI();
-        // instantiate the command used for the autonomous period
-		
-		
-    }
-	
+		// instantiate the command used for the autonomous period
+
+
+	}
+
 	public void disabledPeriodic()
 	{
 		Scheduler.getInstance().run();
 	}
 
-    public void autonomousInit()
-    {
-        // schedule the autonomous command (example)
-        if (autonomousCommand != null) autonomousCommand.start();
-    }
+	public void autonomousInit()
+	{
+		// schedule the autonomous command (example)
+		if (autonomousCommand != null) autonomousCommand.start();
+	}
 
-    /**
-     * This function is called periodically during autonomous
-     */
-    public void autonomousPeriodic()
-    {
-        Scheduler.getInstance().run();
-    }
+	/**
+	 * This function is called periodically during autonomous
+	 */
+	public void autonomousPeriodic()
+	{
+		Scheduler.getInstance().run();
+	}
 
-    public void teleopInit()
-    {
+	public void teleopInit()
+	{
 		// This makes sure that the autonomous stops running when
-        // teleop starts running. If you want the autonomous to 
-        // continue until interrupted by another command, remove
-        // this line or comment it out.
-        if (autonomousCommand != null) autonomousCommand.cancel();
-    }
+		// teleop starts running. If you want the autonomous to 
+		// continue until interrupted by another command, remove
+		// this line or comment it out.
+		if (autonomousCommand != null) autonomousCommand.cancel();
+	}
 
-    /**
-     * This function is called when the disabled button is hit.
-     * You can use it to reset subsystems before shutting down.
-     */
-    public void disabledInit()
-    {
+	/**
+	 * This function is called when the disabled button is hit.
+	 * You can use it to reset subsystems before shutting down.
+	 */
+	public void disabledInit()
+	{
 
-    }
+	}
 
-    /**
-     * This function is called periodically during operator control
-     */
-    public void teleopPeriodic()
-    {
-        Scheduler.getInstance().run();
-    }
-    
-    /**
-     * This function is called periodically during test mode
-     */
-    public void testPeriodic()
-    {
-        LiveWindow.run();
-    }
+	/**
+	 * This function is called periodically during operator control
+	 */
+	public void teleopPeriodic()
+	{
+		Scheduler.getInstance().run();
+	}
 
-    public void operatorControl()
-    {
-        NIVision.IMAQdxStartAcquisition(session);
+	/**
+	 * This function is called periodically during test mode
+	 */
+	public void testPeriodic()
+	{
+		LiveWindow.run();
+	}
 
-        /**
-         * grab an image, draw the circle, and provide it for the camera server
-         * which will in turn send it to the dashboard.
-         */
-        NIVision.Rect rect = new NIVision.Rect(10, 10, 100, 100);
+	public void operatorControl()
+	{
+		NIVision.IMAQdxStartAcquisition(session);
 
-        while (isOperatorControl() && isEnabled())
-        {
+		/**
+		 * grab an image, draw the circle, and provide it for the camera server
+		 * which will in turn send it to the dashboard.
+		 */
+		NIVision.Rect rect = new NIVision.Rect(10, 10, 100, 100);
 
-            NIVision.IMAQdxGrab(session, frame, 1);
-            NIVision.imaqDrawShapeOnImage(frame, frame, rect,
-                    DrawMode.DRAW_VALUE, ShapeMode.SHAPE_OVAL, 0.0f);
-            
-            CameraServer.getInstance().setImage(frame);
+		while (isOperatorControl() && isEnabled())
+		{
 
-            /** robot code here! **/
-            Timer.delay(0.005);		// wait for a motor update time
-        }
-        
-        NIVision.IMAQdxStopAcquisition(session);
-    }
+			NIVision.IMAQdxGrab(session, frame, 1);
+			NIVision.imaqDrawShapeOnImage(frame, frame, rect,
+					DrawMode.DRAW_VALUE, ShapeMode.SHAPE_OVAL, 0.0f);
 
-    public void test()
-    {
-    	//
-    }
+			CameraServer.getInstance().setImage(frame);
+
+			/** robot code here! **/
+			Timer.delay(0.005);		// wait for a motor update time
+		}
+
+		NIVision.IMAQdxStopAcquisition(session);
+	}
+
+	public void test()
+	{
+		//
+	}
 }
---
+++ Sensor-Kesav/src/org/usfirst/frc/team1165/robot/subsystems/ImuDigitalComboBoard.java
@@ -0,0 +1,55 @@
+package org.usfirst.frc.team1165.robot.subsystems;
+
+import edu.wpi.first.wpilibj.ADXL345_I2C;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.I2C.Port;
+import edu.wpi.first.wpilibj.ITG3200;
+import edu.wpi.first.wpilibj.ITG3200.GyroAxis;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.interfaces.Accelerometer;
+
+public class ImuDigitalComboBoard extends Subsystem {
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+	
+	ADXL345_I2C acc;
+	ITG3200 gyro;
+	
+	public ImuDigitalComboBoard(Port port, DigitalInput interrupt)
+	{
+		acc = new ADXL345_I2C(port, Accelerometer.Range.k16G);
+		gyro = new ITG3200(port, interrupt, false);
+	}
+	
+	private GyroAxis xGyro;
+	private GyroAxis yGyro;
+	private GyroAxis zGyro;
+
+    
+	public void initDefaultCommand() 
+	{
+       xGyro = gyro.getXGyro();
+       yGyro = gyro.getYGyro();
+       zGyro = gyro.getZGyro();
+    }
+	public void reportAccelerometer()
+	{
+		SmartDashboard.putNumber("Acc X", acc.getX());
+		SmartDashboard.putNumber("Acc Y", acc.getY());
+		SmartDashboard.putNumber("Acc Z", acc.getZ());
+	}
+	public void reportGyroRate()
+	{
+		SmartDashboard.putNumber("Gyroscope Rate X", xGyro.getAngle());
+		SmartDashboard.putNumber("Gyroscope Rate Y", yGyro.getAngle());
+		SmartDashboard.putNumber("Gyroscope Rate Z", zGyro.getAngle());
+	}
+	public void reportGyroAngle()
+	{
+		SmartDashboard.putNumber("Gyroscope Angle X",xGyro.getRate());
+		SmartDashboard.putNumber("Gyroscope Angle Y", yGyro.getRate());
+		SmartDashboard.putNumber("Gyroscope Angle Z", zGyro.getRate());
+	}
+}
