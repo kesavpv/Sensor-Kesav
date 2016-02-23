7dae926d49c89299be8fd06b6d492e2cedb30ced
kesavpv <kkadalazhi1@pvlearners.net>
2016-02-23 09:28:32 -0700

    Cleaning code

--- Sensor-Kesav/src/org/usfirst/frc/team1165/robot/commands/ReportAccelerometer.java
+++ Sensor-Kesav/src/org/usfirst/frc/team1165/robot/commands/ReportAccelerometer.java
@@ -9,41 +9,33 @@ public class ReportAccelerometer extends Command
 {
 	BuiltInAccelerometer a = new BuiltInAccelerometer();
     
-	public ReportAccelerometer()
-    {
+	public ReportAccelerometer() {
         //requires(Robot.accelerometer);
     }
 
     // Called just before this Command runs the first time
-    protected void initialize()
-    {
+    protected void initialize() {
     }
 
     // Called repeatedly when this Command is scheduled to run
     
-	protected void execute() 
-    {
-    	SmartDashboard.putNumber("Accelerometer X", a.getX());
-    	SmartDashboard.putNumber("Accelerometer Y", a.getY());
-    	SmartDashboard.putNumber("Accelerometer Z", a.getZ());
+	protected void execute() {
+    	Robot.accelerometer.report();
     }
 
     // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished()
-    {
+    protected boolean isFinished() {
         return false;
     }
 
     // Called once after isFinished returns true
-    protected void end()
-    {
+    protected void end() {
     	//
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
-    protected void interrupted()
-    {
+    protected void interrupted() {
     	//
     }
 }
